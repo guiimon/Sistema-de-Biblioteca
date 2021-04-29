0ae023a7487d9bffdb59a3463c2b633f44e93d96
AlexdeJesusFS <65736748+AlexdeJesusFS@users.noreply.github.com>
2021-04-29 15:28:14 -0300

    Update Autores.java

--- Biblioteca/src/biblioteca/Autores.java
+++ Biblioteca/src/biblioteca/Autores.java
@@ -8,15 +8,6 @@ public class Autores {
         this.nome = nome;
     }       
     
-    public void pesquisar(String nome, Biblioteca biblio) {//melhorar
-        for (int i=0; i<=biblio.biblioteca.size(); i++) {
-            if (biblio.biblioteca.get(i).getNome() == nome) {
-                biblio.exibirDados(biblio.biblioteca.get(i));
-            } else {
-                System.out.println("Livro não encontrado. Não cadastrado ou nome incorreto.");
-            }
-        }
-    }
 
     public String getNome() {
         return nome;
